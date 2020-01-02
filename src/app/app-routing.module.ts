4a7ae8d8d14964b06d45f18aabf2832425dad2de
Jimmy <zinuhe@outlook.com>
2020-01-02 00:47:00 -0500

    adding css2 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Css1Component } from './css1/css1.component';
+import { Css2Component } from './css2/css2.component';
 
 // const routes: Routes = [];
 const routes: Routes = [
   // { path: '', redirectTo: '/areas', pathMatch: 'full' },
   { path: 'css1', component: Css1Component },               // CSS1
+  { path: 'css2', component: Css2Component },               // CSS2
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Css1Component } from './css1/css1.component';
+import { Css2Component } from './css2/css2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Css1Component
+    Css1Component,
+    Css2Component
   ],
   imports: [
     BrowserModule,
